adf66fa0d978e358b08e193a0992a46067f1b182
andreiMDL <andreimoda8@gmail.com>
2025-03-08 01:51:16 -0300

    editado configurações de banco de dados e dependencias

--- services/db.adapter.js
+++ services/db.adapter.js
@@ -1,5 +1,5 @@
 const SequelizeAdapter = require("moleculer-db-adapter-sequelize");
 
-const adapter = new SequelizeAdapter("mysql://burgeruser:123456@localhost:3306/make_your_burger");
+const adapter = new SequelizeAdapter("mysql://root:22292254@localhost:3306/make_your_burger");
 
 module.exports = adapter;
